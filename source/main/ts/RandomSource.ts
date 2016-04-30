8481dbcd9fcf37ee0bc2866881c1d907084cd663
Steve Wagner <ciroque@live.com>
2016-04-29 21:35:41 -0700

    Violating a basic tenant, don't check in commented code. But this includes the actual mathematical calculation that works in most cases, but is not correct.

--- source/main/ts/RandomSource.ts
+++ source/main/ts/RandomSource.ts
@@ -4,30 +4,40 @@ import IRandomSource from "./IRandomSource";
 
 export default class RandomSource implements IRandomSource {
     getEmfCount(): number {
-        let emfCount = parseInt(Math.random() * (6 - 1) + 1);
+        let emfCount = Math.floor(Math.random() * (6 - 1) + 1);
         console.log("emfCount = " + emfCount + "\n");
         return emfCount;
     }
     getResistorCount(): number {
-        let resistorCount = parseInt(Math.random() * (12 - 1) + 1);
+        let resistorCount = Math.floor(Math.random() * (12 - 1) + 1);
         console.log("resistorCount = " + resistorCount + "\n");
         return resistorCount;
     }
 
     getVoltage(): number {
-        let voltage = parseInt(Math.random() * (240 - 1) + 1);
+        let voltage = Math.floor(Math.random() * (240 - 1) + 1);
         console.log("voltage = " + voltage + "\n");
         return voltage;
     }
     
-    getResistance(): number {
+    getResistance(iecSeries: number = 12): number {
+        let getDecade = function() {
+            let multipliers = [1, 10, 100, 1000, 10000, 100000, 1000000];
+            let multiplierIndex = Math.floor(Math.random() * multipliers.length);
+            return multipliers[multiplierIndex];
+        };
+
+        // Do it the Maths way. Doesn't work.
+        // let base = parseFloat(Math.pow(10, ( 1 / iecSeries)).toFixed(3));
+        // let decade = getDecade();
+        // let index = Math.floor(Math.random() * (iecSeries - 1) + 1);
+        // let baseResistance = parseFloat(Math.pow(base, index).toFixed(1));
+        // let resistance = baseResistance * decade;
+        // console.log(`>>> BASE(${base}), INDEX(${index}), DECADE(${decade}), BASERESISTANCE(${baseResistance}), RESISTANCE(${resistance})`);
+
         let baseResistances = [1.0, 1.1, 1.2, 1.3, 1.5, 1.6, 1.8, 2.0, 2.2, 2.4, 2.7, 3.0, 3.3, 3.6, 3.9, 4.3, 4.7, 5.1, 5.6, 6.2, 6.8, 7.5, 8.2, 9.1];
-        let multipliers = [1, 10, 100, 1000, 10000, 100000, 1000000];
-
-        let resistanceIndex = parseInt(Math.random() * (baseResistances.length));
-        let multiplierIndex = parseInt(Math.random() * (multipliers.length));
-
-        return baseResistances[resistanceIndex] * multipliers[multiplierIndex];
+        let resistanceIndex = Math.floor(Math.random() * (baseResistances.length));
+        return baseResistances[resistanceIndex] * getDecade();
     }
 
     getAmperage(): number {
