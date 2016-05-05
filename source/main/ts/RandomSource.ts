9fdc1ec3dad38f3c00ffc05010d6f2555c9b5cfa
Steve Wagner <swagner@swagner-mac.local>
2016-05-05 11:32:50 -0700

    Added yet another voltage

--- source/main/ts/RandomSource.ts
+++ source/main/ts/RandomSource.ts
@@ -4,7 +4,7 @@ import IRandomSource from "./IRandomSource";
 
 export default class RandomSource implements IRandomSource {
     standardResistances = [1.0, 1.1, 1.2, 1.3, 1.5, 1.6, 1.8, 2.0, 2.2, 2.4, 2.7, 3.0, 3.3, 3.6, 3.9, 4.3, 4.7, 5.1, 5.6, 6.2, 6.8, 7.5, 8.2, 9.1];
-    standardVoltages = [1.8, 3.3, 5, 9, 12, 15, 18, 24, 48, 120, 240];
+    standardVoltages = [1.8, 3.3, 5, 9, 12, 15, 18, 24, 48, 120, 220, 240];
 
     getEmfCount(): number {
         let emfCount = Math.floor(Math.random() * (6 - 1) + 1);
